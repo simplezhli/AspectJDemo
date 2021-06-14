7b6176952b211775c8359ce7e886adf29963c612
weilu <a05111993@163.com>
2021-06-14 12:54:23 +0800

    init

---
+++ aspectj/src/main/java/com/weilu/aspectj/TraceTagAspectj.java
@@ -0,0 +1,23 @@
+package com.weilu.aspectj;
+
+import android.os.Trace;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+
+@Aspect
+public class TraceTagAspectj {
+
+    @Before("execution(* android.app.Activity+.onCreate(..))")
+    public void before(JoinPoint joinPoint) {
+        Trace.beginSection(joinPoint.getSignature().toString());
+    }
+
+    @After("execution(* android.app.Activity+.onCreate(..))")
+    public void after() {
+        Trace.endSection();
+    }
+}
